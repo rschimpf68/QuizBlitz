9f10223bdd5816b47a3b348d3fce9f3d66cdf057
Ronald Schimpf <ronald.schimpf@uap.edu.ar>
2023-05-30 12:19:36 -0300

    Auth Session

--- src/app/api/register/route.jsx
+++ src/app/api/register/route.jsx
@@ -1,35 +1,35 @@
-import bcrypt from 'bcrypt'
-import prisma from '../../../../lib/prisma'
-import { NextApiResponse, NextApiRequests } from 'next'
-import { NextResponse } from 'next/server';
+import bcrypt from "bcrypt";
+import prisma from "../../libs/prismadb";
+import { NextApiResponse, NextApiRequests } from "next";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const body = await request.json();
-    const { name, email, password } = body;
+  const body = await request.json();
+  const { name, email, password } = body;
 
-    if (!name || !email || !password) {
-        return new NextResponse('Missing Fields', { status: 400 })
-    }
+  if (!name || !email || !password) {
+    return new NextResponse("Missing Fields", { status: 400 });
+  }
 
-    const exist = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    });
+  const exist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if (exist) {
-        throw new Error('Email already exists')
-    }
+  if (exist) {
+    throw new Error("Email already exists");
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
-        data: {
-            email,
-            name,
-            hashedPassword
-        }
-    });
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
 
-    return NextResponse.json(user)
+  return NextResponse.json(user);
 }
